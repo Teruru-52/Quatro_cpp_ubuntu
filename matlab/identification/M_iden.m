390a449a7fb4265a6a0fa540683d46a2e0e4b710
Reiji <reiji.210@icloud.com>
2022-07-05 18:24:14 +0900

    identification ok

--- matlab/identification/M_iden.m
+++ matlab/identification/M_iden.m
@@ -1,5 +1,8 @@
 % M系列信号によるシステム同定
 %% M系列信号入力と応答
+close all
+clear
+
 data = csvread('data.csv');
 
 input = data(:, 1);
@@ -11,7 +14,7 @@ plot(input,'LineWidth',2);
 grid on
 xlabel('Time n','Interpreter','latex','FontSize',20);
 ylabel('$u_\omega$ [V]','Interpreter','latex','FontSize',20);
-xlim([0 1270]);
+xlim([0 2540]);
 ylim([-1.7 1.7]);
 h_axes = gca;
 h_axes.XAxis.FontSize = 20;
@@ -22,28 +25,18 @@ plot(output,'LineWidth',2);
 grid on;
 xlabel('Time n','Interpreter','latex','FontSize',20);
 ylabel('$\omega$ [rad/s]','Interpreter','latex','FontSize',20);
-xlim([0 1270]);
+xlim([0 2540]);
 h_axes = gca;
 h_axes.XAxis.FontSize = 20;
 h_axes.YAxis.FontSize = 20;
 %% システム同定
+exp_input = input(1:1270,1);
+val_input = input(1271:2540,1);
+exp_output = output(1:1270,1);
+val_output = output(1271:2540,1);
 systemIdentification
-
-%% 
-Ts = 0.001;
-Num = tf1.Numerator;
-Den = tf1.Denominator;
-% sys = tf(Num, Den)
-ff = Den / Num;
-tf1
-% dis_tf1 = c2d(sys,Ts,'zoh')
-%%
-Ts = 0.001;
-Num2 = tf2.Numerator;
-Den2 = tf2.Denominator;
-% sys2 = tf(Num2, Den2)
-ff2 = Den2 / Num2
-tf2
-% dis_tf2 = c2d(sys2,Ts)
+%% 2022/07/05
+% Tp1 = 20.56
+% Kp = 137.9
 %%
 pidTuner()
--- matlab/identification/M_sequence.m
+++ matlab/identification/M_sequence.m
@@ -14,3 +14,18 @@ ylabel('$u_\omega$ [V]','Interpreter','latex','FontSize',20);
 h_axes = gca;
 h_axes.XAxis.FontSize = 20;
 h_axes.YAxis.FontSize = 20;
+%% 検証用入力の結合
+input = input(1:127,1);
+% 半周期だけずらして結合する
+input_formar = input(1:63,1);
+input_latter = input(64:127,1);
+validation_input = cat(1, input_latter, input_formar);
+
+% 実験・検証用入力
+input2 = cat(1,input,validation_input);
+stairs(0:2*N-1,input2,'LineWidth',3)
+grid on
+xlim([0 2*N-1])
+ylim([-1.7 1.7])
+xlabel('Time n','Interpreter','latex','FontSize',20);
+ylabel('$u_\omega$ [V]','Interpreter','latex','FontSize',20);
